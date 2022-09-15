9e4536648cc96b67c062542427bfec0fb9be90d7
Nur Fajar Sayyidul Ayyam <nur.fajar11@ui.ac.id>
2022-09-15 13:00:35 +0700

    develop show_xml function

--- wishlist/views.py
+++ wishlist/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.shortcuts import render
 from wishlist.models import BarangWishlist
+from django.http import HttpResponse
+from django.core import serializers
 
 # Create your views here.
 def show_wishlist(request):
@@ -10,3 +12,7 @@ def show_wishlist(request):
         'nama': 'Nur Fajar'
     }
     return render(request, "wishlist.html", context)
+
+def show_xml(request):
+    data = BarangWishlist.objects.all()
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
